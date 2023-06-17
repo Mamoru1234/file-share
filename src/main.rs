c000c5d870a3bb247d27efe77e368d57f0e702aa
Oleksii Gontar <f.gontar5@gmail.com>
2023-06-17 12:55:49 +0300

    minor improvements

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{App, HttpServer};
 use actix_files::Files;
+use actix_web::{App, HttpServer};
 use file_share::listing_renderer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(
-            Files::new("/", ".")
+            Files::new("/", "./data")
                 .show_files_listing()
                 .redirect_to_slash_directory()
-                .files_listing_renderer(listing_renderer)
+                .files_listing_renderer(listing_renderer),
         )
     })
     .bind(("0.0.0.0", 8080))?
