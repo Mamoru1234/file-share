4262ced4fff42f16ebcb52c1d918ce91ac9e7af4
Oleksii Gontar <f.gontar5@gmail.com>
2023-06-24 17:07:16 +0300

    minor improvements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use actix_files::Files;
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 use file_share::listing_renderer;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     HttpServer::new(|| {
+        let data_dir = env::var("DATA_DIR").unwrap_or(String::from("./data"));
         App::new().service(
-            Files::new("/", "./data")
+            Files::new("/", data_dir)
                 .show_files_listing()
                 .redirect_to_slash_directory()
                 .files_listing_renderer(listing_renderer),
